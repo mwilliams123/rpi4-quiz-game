7d3081f123e41b5330e3b1c543fd1ecf0c1bebeb
Megan Williams <wilm140@ucla.edu>
2022-02-12 15:23:01 -0700

    Add RPi hardware integration

--- game.py
+++ game.py
@@ -4,12 +4,15 @@ from constants import GameState, Colors
 from title import title_screen
 from loading import load_data, loading_screen
 from question import blit_text
+from hardware import green_light, ready
 
 # load pygame screen
 pygame.init()
 screen = pygame.display.set_mode((700,700))
 game_state = GameState.TITLE
 loading_thread = None
+green = False
+ready()
 
 # Main loop
 while game_state is not GameState.QUIT:
@@ -34,6 +37,9 @@ while game_state is not GameState.QUIT:
     if game_state is GameState.BOARD:
         game_state = draw_board(screen, mouse_click)
     if game_state is GameState.QUESTION:
+        if not green:
+            green_light()
+            green = True
         screen.fill(Colors.BLUE)
         blit_text(screen, "This is a question")
 
