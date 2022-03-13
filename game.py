2a134611e1eb9a6410fabb59018e402e5c041616
Megan Williams <wilm140@ucla.edu>
2022-03-13 00:49:12 -0700

    Display score

--- game.py
+++ game.py
@@ -7,11 +7,14 @@ from loading import load_data, loading_screen
 from question import draw_question, draw_answer
 from categories import show_categories
 from util import load_fonts
+from score import display_score
 #from hardware import green_light, ready
 
 # load pygame screen
 pygame.init()
-screen = pygame.display.set_mode((1000,700))
+screen = pygame.display.set_mode((1300,700))
+game_board = pygame.Surface((1000, 700))
+score = pygame.Surface((300, 700))
 game_state = GameState.TITLE
 loading_thread = None
 green = False
@@ -32,26 +35,30 @@ while game_state is not GameState.QUIT:
 
     # Draw game
     if game_state is GameState.TITLE:
-        game_state = title_screen(screen, mouse_click)
+        game_state = title_screen(game_board, mouse_click)
     if game_state is GameState.LOADING:
         if loading_thread is None:
             loading_thread = load_data()
-        game_state, store = loading_screen(screen, loading_thread, store)
+        game_state, store = loading_screen(game_board, loading_thread, store)
     if game_state is GameState.INTRO:
-        game_state = show_categories(screen, store)
+        game_state = show_categories(game_board, store)
     if game_state is GameState.BOARD:
-        game_state, store = draw_board(screen, mouse_click, store)
+        game_state, store = draw_board(game_board, mouse_click, store)
     if game_state is GameState.QUESTION:
         if not store['green']:
             pm.green_light()
             store['green'] = True
         game_state, store = pm.poll(store)
-        draw_question(screen, store)
+        draw_question(game_board, store)
     if game_state is GameState.ANSWER:
-        game_state = draw_answer(screen, store, pm, mouse_click)
+        game_state = draw_answer(game_board, store, pm, mouse_click)
     if game_state is GameState.FINAL:
         pass
         #game_state = final(screen, store)
+    if game_state != GameState.TITLE and game_state != GameState.LOADING:
+        display_score(score, store, pm)
+    screen.blit(game_board, (0,0))
+    screen.blit(score, (1000,0))
     # Display screen
     pygame.display.flip()
         
--- score.py
+++ score.py
@@ -1,8 +1,23 @@
+from constants import Colors
+import pygame
 
-def draw_score(rect, score)
+def draw_score(screen, rect, font, score, i):
+    text = font.render('$' + str(score), True, Colors.WHITE)
+    text_rect = text.get_rect(center=((rect[0] + rect[2])/2, rect[1] + rect[3]*3/4))
+    screen.blit(text,text_rect)
 
-def display_score(screen, pm):
-    rect = (1000,1400, 0, 700)
+    text = font.render('Player ' + str(i), True, Colors.WHITE)
+    text_rect = text.get_rect(center=((rect[0] + rect[2])/2, rect[1] + rect[3]/4))
+    screen.blit(text,text_rect)
+
+
+def display_score(screen, store, pm):
+    width, height = screen.get_size()
+    height = height / 3
+    screen.fill(Colors.BLUE)
+    font = store['fonts']['number']
     for i,p in enumerate(pm.players):
-        draw_score((), p.score)
+        rect = (0, i*height, width, height) # left top right bottom
+        draw_score(screen, rect, font, p.score, i+1)
+        pygame.draw.rect(screen, Colors.BLACK, rect, 5)
     
