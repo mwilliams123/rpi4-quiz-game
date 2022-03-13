1255cf6c1eaa5bf5ab13de5f40611409582cacbb
Megan Williams <wilm140@ucla.edu>
2022-03-13 15:05:36 -0600

    Fix final jeopardy bug

--- final.py
+++ final.py
@@ -7,16 +7,22 @@ def final(screen, store, mouse_click):
     w, h = screen.get_size()
     clue = store['data']['fj']
     text = ''
+    et = store['clock'].tick()
     if not store['wagers']:
         text = clue['category']
         font = pygame.font.SysFont("arial", 40)
         text_rect = font.render('Continue', True, Colors.WHITE)
         rect = text_rect.get_rect(center=(w*1/2, h*3/4))
         screen.blit(text_rect,rect)
+        
         if mouse_click and rect.collidepoint(pygame.mouse.get_pos()):
             store['wagers'] = True
+            
+        final_rect = store['fonts']['number'].render('Final Jeopardy', True, Colors.GOLD)
+        rect = final_rect.get_rect(center=(w*1/2, h/4))
+        screen.blit(final_rect,rect)
     else:
-        et = store['clock'].tick()
+        
         store['timer'] = store['timer'] - et
         if store['timer'] < 0:
             text = clue['question']
